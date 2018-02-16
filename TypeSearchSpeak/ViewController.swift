528b463ba3dca618f21df8b611cf4af05e1e9a6b
Robbie Perry <robbieperry22@gmail.com>
2018-02-16 14:35:38 -0800

    Initial Commit

---
+++ TypeSearchSpeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypeSearchSpeak
+//
+//  Created by Robbie Perry on 2018-02-16.
+//  Copyright © 2018 Robbie Perry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
